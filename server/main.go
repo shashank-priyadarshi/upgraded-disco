806ae8f42d847e72738c7e410b40836b585cdf3f
shashank-priyadarshi <shashank9163882019@gmail.com>
2022-12-04 15:20:42 +0530

    New endpoints for todos and articles MongoDB connection enabled

--- server/main.go
+++ server/main.go
@@ -1,108 +1,105 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
 	"net/http"
-	"os"
 )
 
-type dummyData struct {
-	Header struct {
-		Name string   `json:"name"`
-		Role []string `json:"role"`
-	} `json:"header"`
-	Footer struct {
-		Links []struct {
-			Linkedin string `json:"linkedin"`
-			Github   string `json:"github"`
-		} `json:"links"`
-	} `json:"footer"`
-	Body struct {
-		Summary         []string `json:"summary"`
-		CareerObjective string   `json:"career_objective"`
-		Skillsets       struct {
-			TechnicalSkillGrid []string `json:"technical_skill_grid"`
-		} `json:"skillsets"`
-		Education struct {
-			College   []string `json:"college"`
-			Secondary []string `json:"secondary"`
-		} `json:"education"`
-		EmploymentProfile struct {
-			CurrentEmployer  []string      `json:"current_employer"`
-			PreviousEmployer []interface{} `json:"previous_employer"`
-		} `json:"employment_profile"`
-		Projects       [][]string    `json:"projects"`
-		Certifications []interface{} `json:"Certifications"`
-	} `json:"body"`
-	Keys struct {
-		Header []string `json:"header"`
-		Footer []string `json:"footer"`
-		Links  []string `json:"links"`
-		Body   struct {
-			Skillsets []struct {
-				TechnicalSkillGrid []string `json:"technical_skill_grid"`
-			} `json:"skillsets"`
-			Education []struct {
-				College   []string `json:"college,omitempty"`
-				Secondary []string `json:"secondary,omitempty"`
-			} `json:"education"`
-			EmploymentProfile struct {
-				Employer []string `json:"employer"`
-			} `json:"employment_profile"`
-			Projects       []string `json:"projects"`
-			Certifications struct {
-			} `json:"Certifications"`
-		} `json:"body"`
-	} `json:"keys"`
-	Assets            []interface{} `json:"assets"`
-	EmploymentProfile struct {
-		CurrentEmployer struct {
-			Employer       string `json:"employer"`
-			Designation    string `json:"designation"`
-			DateOfJoining  string `json:"date_of_joining"`
-			OfficeLocation string `json:"office_location"`
-			Description    string `json:"description"`
-		} `json:"current_employer"`
-		PreviousEmployer []interface{} `json:"previous_employer"`
-	} `json:"employment_profile"`
-}
+var db *mongo.Database
 
 func handleRequests() {
 	//myRouter := mux.NewRouter().StrictSlash(true)
 	//mux.NewRouter().StrictSlash(true)
 	//http.HandleFunc("/", homePage)
 	http.HandleFunc("/biodata", returnBiodata)
+	http.HandleFunc("/todos", returnTodos)
+	http.HandleFunc("/articles", returnArticles)
 	//myRouter.HandleFunc("/newdata", editData).Methods("POST")
 	log.Fatal(http.ListenAndServe(":10000", nil))
 }
 
-func readData() dummyData {
-	rawData, err := os.ReadFile("data.json")
-	if err != nil {
-		fmt.Println(err)
-	}
-	var unmarshalledData dummyData
-	err = json.Unmarshal(rawData, &unmarshalledData)
-	if err != nil {
-		fmt.Println(err)
-	}
-	return unmarshalledData
-}
-
 func returnBiodata(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Endpoint Hit: returnAllData")
-	json.NewEncoder(w).Encode(readData())
+	fmt.Println("Endpoint Hit: returnBiodata")
+	json.NewEncoder(w).Encode(readDataFromCollection(db.Collection("resume")))
 }
 
-//func editData(w http.ResponseWriter, r *http.Request) {
-//	// get the body of our POST request
-//	// return the string response containing the request body
-//	reqBody, _ := io.ReadAll(r.Body)
-//	fmt.Fprintf(w, "%+v", string(reqBody))
-//}
+func returnTodos(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: returnTodos")
+	json.NewEncoder(w).Encode(readDataFromCollection(db.Collection("todo")))
+}
+
+func returnArticles(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: returnArticles")
+	json.NewEncoder(w).Encode(readDataFromCollection(db.Collection("articles")))
+}
+
+func setMongoConnection() *mongo.Client {
+	// Set client options
+	clientOptions := options.Client().ApplyURI("mongodb://user:password@<URI>:27017/")
+
+	// Connect to MongoDB
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Check the connection
+	err = client.Ping(context.TODO(), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return client
+}
+
+func readDataFromCollection(collection *mongo.Collection) []interface{} {
+	// Pass these options to the Find method
+	findOptions := options.Find()
+	//findOptions.SetLimit(2)
+
+	// Here's an array in which you can store the decoded documents
+	var results []interface{}
+
+	// Passing bson.D{{}} as the filter matches all documents in the collection
+	cur, err := collection.Find(context.TODO(), bson.D{{}}, findOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Finding multiple documents returns a cursor
+	// Iterating through the cursor allows us to decode documents one at a time
+	for cur.Next(context.TODO()) {
+		// create a value into which the single document can be decoded
+		var elem *interface{}
+		err := cur.Decode(&elem)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		results = append(results, *elem)
+	}
+
+	if err := cur.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	// Close the cursor once finished
+	cur.Close(context.TODO())
+
+	return results
+}
 
 func main() {
+	client := setMongoConnection()
+	db = client.Database("portfolio")
+
 	handleRequests()
 }
