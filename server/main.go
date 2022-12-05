c25f546970aeb044fb8f62164dde974abe34b9c4
shashank-priyadarshi <shashank9163882019@gmail.com>
2022-12-05 21:58:52 +0530

    Disabled Access control allow origin, headers and method enabled, Added docker-compose.yml & Dockerfile

--- server/main.go
+++ server/main.go
@@ -26,31 +26,31 @@ func handleRequests() {
 
 func returnBiodata(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint Hit: returnBiodata")
-	enableCors(&w)
+	//enableCors(&w)
 	json.NewEncoder(w).Encode(readDataFromCollection(db.Collection("resume")))
 }
 
 func returnTodos(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint Hit: returnTodos")
-	enableCors(&w)
+	//enableCors(&w)
 	json.NewEncoder(w).Encode(readDataFromCollection(db.Collection("todo")))
 }
 
 func returnArticles(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint Hit: returnArticles")
-	enableCors(&w)
+	//enableCors(&w)
 	json.NewEncoder(w).Encode(readDataFromCollection(db.Collection("articles")))
 }
 
-func enableCors(w *http.ResponseWriter) {
-	(*w).Header().Set("Access-Control-Allow-Origin", "*")
-	(*w).Header().Set("Access-Control-Allow-Methods", "GET")
-	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
-}
+//func enableCors(w *http.ResponseWriter) {
+//(*w).Header().Set("Access-Control-Allow-Origin", "*")
+//(*w).Header().Set("Access-Control-Allow-Methods", "GET")
+//(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+//}
 
 func setMongoConnection() *mongo.Client {
 	// Set client options
-	clientOptions := options.Client().ApplyURI("mongodb://user:password@<URI>:27017/")
+	clientOptions := options.Client().ApplyURI("mongodb://root:pass12345@192.46.212.144:27017/?authSource=admin")
 
 	// Connect to MongoDB
 	client, err := mongo.Connect(context.TODO(), clientOptions)
@@ -65,7 +65,7 @@ func setMongoConnection() *mongo.Client {
 	if err != nil {
 		log.Fatal(err)
 	}
-
+	fmt.Println("MongoDB connection succeeded:")
 	return client
 }
 
