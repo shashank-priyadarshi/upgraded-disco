847536f2028ee2b9a033ea6004c4c78342ba563d
shashank-priyadarshi <shashank9163882019@gmail.com>
2023-02-05 21:14:39 +0530

    server endpoint methods updated, comments removed

--- server/endpoints.go
+++ server/endpoints.go
@@ -6,7 +6,6 @@ import (
 	"net/http"
 	"server/common"
 	"server/config"
-	"server/todos"
 
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
@@ -15,23 +14,14 @@ import (
 func routes() *mux.Router {
 	r := mux.NewRouter()
 	r.HandleFunc("/biodata", returnBiodata).Methods("GET")
-	r.HandleFunc("/trigger", triggerPlugin).Methods("GET")
-	r.HandleFunc("/todos", todos.ReturnTodos).Methods("GET")
 	r.HandleFunc("/githubdata", returnGitHubData).Methods("GET")
+	r.HandleFunc("/todos", todos).Methods("POST")
+	r.HandleFunc("/trigger", triggerPlugin).Methods("POST")
 	r.HandleFunc("/graphql", graphqlHandler).Methods("POST")
 	r.HandleFunc("/schedule", writeNewSchedule).Methods("POST")
 	r.NotFoundHandler = http.HandlerFunc(common.InvalidEndpoint)
 	return r
 }
-
-//func originValidator(origin string) bool {
-//	valid := false
-//	err := pool.QueryRow("SELECT IF(origin=?, True, False) as 'valid' FROM origins", origin).Scan(&valid)
-//	if err != nil {
-//		return false
-//	}
-//	return valid
-//}
 
 func handleRequests() {
 	router := routes()
