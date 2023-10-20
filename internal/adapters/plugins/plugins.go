bdba74becc2d7ff49f90ad75d4a2f9670f1db606
Shashank Priyadarshi <57692808+shashank-priyadarshi@users.noreply.github.com>
2023-10-20 11:16:50 +0000

    Plugin interfaces scaffolding refactored and comments added

--- internal/adapters/plugins/plugins.go
+++ internal/adapters/plugins/plugins.go
@@ -6,4 +6,4 @@ func(p *Plugin)Install(plugin string) error{return nil}
 func(p *Plugin)Uninstall(plugin string) error{return nil}
 func(p *Plugin)Upgrade(plugin string) error{return nil}
 func(p *Plugin)Trigger(plugin string, payload interface{}) error{return nil}
-func(p *Plugin)GetLastTrigger() interface{}{return nil}
+func(p *Plugin)GetLastTrigger(plugin string, payload interface{}) interface{}{return nil}
--- internal/ports/plugins/plugin.go
+++ internal/ports/plugins/plugin.go
@@ -2,11 +2,11 @@ package plugin
 
 
 type Plugin interface{
-	Install(plugin string) error
-	Uninstall(plugin string) error
-	Upgrade(plugin string) error
-	Trigger(plugin string, payload interface{}) error
-	GetLastTrigger() interface{}
+	Install(plugin string) error // Build the corresponding folder given the plugin name
+	Uninstall(plugin string) error // Remove the built binary
+	Upgrade(plugin string) error // Download latest code and rebuild the binary
+	Trigger(plugin string, payload interface{}) error // Trigger the plugin
+	GetLastTrigger(plugin string, payload interface{}) interface{} // Get the plugin trigger based on name,  count and failed/successful executions
 }
 
 type PluginWrapper interface{}
